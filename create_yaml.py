11a2711a11b444468ada98d998e0ea9c0d7b74f2
Michael Nikitenko <xm4dn355x@gmail.com>
2020-09-17 10:54:03 +0500

    script changed to callable module

--- create_yaml.py
+++ create_yaml.py
@@ -11,39 +11,44 @@
 
 import yaml
 
+GROUPS = [
+    {
+        'name': 'В Салехарде',
+        'city': 'г. Салехард',
+        'url': 'https://vk.com/inshd'
+    },
+    {
+        'name': 'Подслушано в Салехарде',
+        'city': 'г. Салехард',
+        'url': 'https://vk.com/hearsalehard'
+    },
+    {
+        'name': 'В Салехарде.ru',
+        'city': 'г. Салехард',
+        'url': 'https://vk.com/vsaleharderu'
+    },
+    {
+        'name': 'Салехард',
+        'city': 'г. Салехард',
+        'url': 'https://vk.com/public124643438'
+    },
+    {
+        'name': 'Афиша В Салехарде',
+        'city': 'г. Салехард',
+        'url': 'https://vk.com/afisha_shd'
+    },
+]
 
-if __name__ == '__main__':
-    print('YAML creator.')
-    settings = [
-        {
-            'name': 'В Салехарде',
-            'city': 'г. Салехард',
-            'url': 'https://vk.com/inshd'
-        },
-        {
-            'name': 'Подслушано в Салехарде',
-            'city': 'г. Салехард',
-            'url': 'https://vk.com/hearsalehard'
-        },
-        {
-            'name': 'В Салехарде.ru',
-            'city': 'г. Салехард',
-            'url': 'https://vk.com/vsaleharderu'
-        },
-        {
-            'name': 'Салехард',
-            'city': 'г. Салехард',
-            'url': 'https://vk.com/public124643438'
-        },
-        {
-            'name': 'Афиша В Салехарде',
-            'city': 'г. Салехард',
-            'url': 'https://vk.com/afisha_shd'
-        },
-    ]
+
+def create_yaml():
     print('\nTrying to open yaml file')
     f = open('groups.yml', 'w')
     print('Saving data...')
-    f.write(yaml.dump(settings))
+    f.write(yaml.dump(GROUPS))
     f.close()
-    print('Setting saved to bots.yaml successfully.')
+    print('Data saved to groups.yml successfully.')
+
+
+if __name__ == '__main__':
+    print('YAML creator.')
+    create_yaml()
