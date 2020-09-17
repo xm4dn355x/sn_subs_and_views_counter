ee7b5d64ebf8fdc33cdbaa321678016f2c0a59ae
Michael Nikitenko <xm4dn355x@gmail.com>
2020-09-17 10:25:39 +0500

    finished parser script

--- parser.py
+++ parser.py
@@ -9,20 +9,11 @@
 #####################################################################################################################
 
 
-from bs4 import BeautifulSoup
-from requests import get
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 import yaml
-
-
-def get_html(url):
-    """Создает GET-запрос и возвращает HTML"""
-    try:
-        r = get(url, headers={'User-Agent': 'Custom'})  # Создаем объект web-страницы по полученному url
-        print(r, end=" ")  # Ответ от сервера <Response [200]>
-        return r.text
-    except :
-        print('Ошибка подключения')
-        return ''   # Возвращаем пустую строку вместо HTML
 
 
 def get_groups_list():
@@ -32,5 +23,36 @@ def get_groups_list():
     return groups
 
 
+def get_group_subs_number(driver, url):
+    """Получает HTML и находит в нём количество подписчиков"""
+    driver.get(url)
+    try:
+        res = int(driver.find_element_by_class_name('group_friends_count').text.strip())
+    except NoSuchElementException:
+        res = int(driver.find_element_by_class_name('header_count').text.replace(" ", '').strip())
+    return res
+
+
+def prepare_data(driver, groups_list):
+    """Получает список групп с ссылками на них и возвращает список словарей {"название" "кол-во подписчиков"}"""
+    res = []
+    for group in groups_list:
+        subs = get_group_subs_number(driver, group['url'])
+        res.append({'name': group['name'], 'subs': subs})
+    return res
+
+
+def get_all_subs_numbers():
+    """Основная функция возвращающая список групп с их количеством подписчиков"""
+    groups_list = get_groups_list()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://vk.com/hearsalehard")
+    sleep(5)
+    return prepare_data(driver, groups_list)
+
+
 if __name__ == '__main__':
-    pass
+    print("Парсер групп вконтакте с целью поиска количества подписчиков")
+    data = get_all_subs_numbers()
+    print(data)
+
