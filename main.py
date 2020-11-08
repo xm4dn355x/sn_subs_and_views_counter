b0f14749b4e99bc201314f8c1dd4bc89cba4d7c0
Michael Nikitenko <xm4dn355x@gmail.com>
2020-11-08 16:28:54 +0500

    created skeletons for parsers

--- main.py
+++ main.py
@@ -10,6 +10,13 @@
 
 
 from create_yamls import create_all_yamls
+from sn_parsers.fb_parser import parse_fb
+from sn_parsers.ig_parser import parse_ig
+from sn_parsers.mm_parser import parse_mm
+from sn_parsers.ok_parser import parse_ok
+from sn_parsers.tg_parser import parse_tg
+from sn_parsers.vk_parser import parse_vk
+from sn_parsers.yt_parser import parse_yt
 
 
 # Globals
@@ -21,3 +28,17 @@ XLSX_ROWS_COUNT = 81
 if __name__ == '__main__':
     print("VK Group list subscribers counter")
     create_all_yamls(csv_path=CSV_PATH, xlsx_path=XLSX_PATH, xlsx_rows_count=XLSX_ROWS_COUNT)
+    fb_data = parse_fb()
+    ig_data = parse_ig()
+    mm_data = parse_mm()
+    ok_data = parse_ok()
+    tg_data = parse_tg()
+    vk_data = parse_vk()
+    yt_data = parse_yt()
+    print(fb_data)
+    print(ig_data)
+    print(mm_data)
+    print(ok_data)
+    print(tg_data)
+    print(vk_data)
+    print(yt_data)
